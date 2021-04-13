c49195d6f51a8283d3f35d7e7523eadbd483c557
Romulo Albanus <romuloaalbanus@hotmail.com>
2021-04-13 13:59:36 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0; //ou this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items = [...this.items, item].sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items < pos) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if ((this.length = 0)) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.pop();
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.shift();
+    }
+  }
 
-  sum() {}
+  sum() {
+    let total = 0;
+    if (this.length == 0) {
+      return 0;
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        total += this.items[i];
+      }
+      return total;
+    }
+  }
 
-  avg() {}
+  avg() {
+    let total = 0;
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        total += this.items[i];
+      }
+      return total / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
